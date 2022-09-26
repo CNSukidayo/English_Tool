ce60862e62ed9adbc62e2d1439e8a079bacd78ca
sukidayo <2985384723@qq.com>
2022-09-26 21:13:10 +0800

    test git

--- src/main/java/com/bilibili/MainUI.java
+++ src/main/java/com/bilibili/MainUI.java
@@ -8,6 +8,7 @@ import com.bilibili.ui.EnglishToolFrame;
  */
 public class MainUI {
     public static void main(String[] args) {
+        System.out.println("Hello Git!");
         EnglishToolFrame englishToolFrame = new EnglishToolFrame();
         englishToolFrame.run();
     }
